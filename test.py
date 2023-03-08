02e064ca853cbf4e4f564fad309589de0c848ae8
Lindsey <122494588+18leaf@users.noreply.github.com>
2023-03-08 22:22:24 +0000

    Wed, Mar 8, 2023, 10:22 PM +00:00

--- test.py
+++ test.py
@@ -30,8 +30,7 @@ def load_audio(filepath):
 
     try:
         sound = AudioSegment.from_wav(filepath)
-        samples = sound.get_array_of_samples()
-        samples = np.array(samples)
+        samples = np.array(sound.get_array_of_samples())
     except FileNotFoundError:
         sys.exit(f"ERROR: input.wav not found")
 
