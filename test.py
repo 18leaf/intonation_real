09edef1c0c7c6b3978769cc39c6ad1d825ef60f5
Lindsey <122494588+18leaf@users.noreply.github.com>
2023-03-09 14:08:03 +0000

    Thu, Mar 9, 2023, 2:08 PM +00:00

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ def analyze_audio(samples):
         })"""
 
 
-    sample_rate = AudioSegment.from_wav(sys.argv[1]).frame_rate
+    sample_rate = AudioSegment.from_wav(sys.argv[1]).fra
     window_size = 2048
     hop_size = 256
     analyzed_data = []
