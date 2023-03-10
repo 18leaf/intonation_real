17ab1d2df1e74e0cd417c95d04b6027cf636a70a
Lindsey <122494588+18leaf@users.noreply.github.com>
2023-03-10 17:01:41 +0000

    Fri, Mar 10, 2023, 5:01 PM +00:00

--- main.py
+++ main.py
@@ -3,79 +3,116 @@ from pydub import AudioSegment  # reference at bottom
 import csv
 import sys
 
-def main():
-    filepath = input("Please enter the path to your audio file: ")
-    try:
+try:
         with open("tuning.csv") as csvfile:
             reader = csv.DictReader(csvfile)
             tuning = {row['Note']: float(row['Frequency']) for row in reader} # create dictionary for all correct note values.. {"Note name": frequency}
-    except FileNotFoundError:
-        sys.exit("tuning.csv not found")
+except FileNotFoundError:
+        sys.exit("ERROR: tuning.csv not found")
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("USAGE: python main.py input.wav")
+
+    filepath = sys.argv[1]
+
     samples = load_audio(filepath)
-    print(samples)
     analyzed_data = analyze_audio(samples)
-    scores = calculate_accuracy(analyzed_data)
-    print(scores)
+    # note_scores = calculate_accuracy(analyzed_data)
+    # mse = calculate_mse(analyzed_data)
+    pererror = calculate_percent_error(analyzed_data)
+    # print("Per-note accuracy scores:")
+    # print(note_scores)
+    # print(f"MSEcalc = {mse}")
+    # print(f"Percent Error = {pererror}")
+    print(f"\nIntonation Score for {filepath}\n")
+    for note in pererror:
+        if pererror[note] < 20:
+            continue
+        print(f"{note} - {round((pererror[note]), 2)}%")
 
 
 def load_audio(filepath):
     """
     # Load audio file from disk and return as numpy array.
     """
-    sound = AudioSegment.from_wav(filepath)
-    samples = sound.get_array_of_samples()
-    samples = np.array(samples)
-    return samples
 
+    try:
+        sound = AudioSegment.from_wav(filepath)
+        samples = np.array(sound.get_array_of_samples())
+    except FileNotFoundError:
+        sys.exit(f"ERROR: input.wav not found")
+
+    return samples
 
 def get_closest_pitch(freq):
     """
     # Given a frequency, returns the closest note in the key by frequency.
     """
-    # Implement your code to find the closest note here
-    # You may want to store the frequencies of the notes in the key as a dictionary
+
+    closest_note = ""
+    min_distance = float("inf") # set min distance to infinity, so all distance are less
+    for Note, Frequency in tuning.items(): # itereate of each item in the tuning dict created from csv
+         distance = abs(freq - Frequency) # distance is given pitch(found later in analyze audio) from csv tunign
+         if distance < min_distance: # pseudo sort to find closest note to actual pitch
+              min_distance = distance
+              closest_note = Note
+
+    return closest_note
 
 
 def analyze_audio(samples):
     """
-    # Given a numpy array of audio samples, analyzes the pitch of each note and returns a list of dicts with the analyzed data for each note.
+    # Given a numpy array of audio samples, analyzes the pitch of each note and returns a
+    list of dicts with the analyzed data for each note.
     """
+
+    sample_rate = AudioSegment.from_wav(sys.argv[1]).frame_rate
+    window_size = 4096
+    hop_size = 256
     analyzed_data = []
-    # Implement your code to analyze the pitch of each note here
-    # You may want to use the get_closest_pitch function here
+
+    for i in range(0, len(samples) - window_size, hop_size):
+        window = samples[i:i+window_size] * np.hamming(window_size)
+        # Apply FFT to the window
+        spectrum = np.abs(np.fft.fft(window))
+        # Get the frequency values of the spectrum
+        freqs = np.fft.fftfreq(window_size, d=1/sample_rate)[:window_size//2]
+        # Find the index of the peak frequency
+        peak_index = np.argmax(spectrum[:window_size//2])
+        # Convert the index to a pitch value in Hz
+        pitch = freqs[peak_index]
+        # Find the closest pitch name to the calculated pitch value
+        note = get_closest_pitch(pitch)
+        start_time = i / sample_rate
+        duration = window_size / sample_rate
+        analyzed_data.append({
+            "note": note,
+            "start_time": start_time,
+            "duration": duration,
+            "pitch": pitch
+        })
 
     return analyzed_data
 
 
-def calculate_accuracy(analyzed_data):
-    """
-    # Given a list of analyzed data for each note, calculates the accuracy of intonation for each note and returns a dict with the scores for each note.
-    """
-    scores = {}
-    # Implement your code to calculate the accuracy of intonation for each note here
+def calculate_percent_error(analyzed_data):
+    error_scores = {}
+    for note, freq in tuning.items():
+        notedata = [d for d in analyzed_data if d["note"] == note]
+        if len(notedata) == 0:
+            # If there is no analyzed data for the current note, skip it
+            continue
+        # Calculate the mean pitch value for the current note
+        pitch_values = [d["pitch"] for d in notedata]
+        mean_pitch = sum(pitch_values) / len(pitch_values)
+        # Calculate the percent error between the mean pitch value and the expected frequency
+        percent_error = abs((mean_pitch - freq) / freq) * 100
+        error_scores[note] = 100 - percent_error
 
-    return scores
+    return error_scores
+
 
 if __name__ == "__main__":
     main()
-
-
-"""
-pydub:
-
-    AudioSegment.from_wav(filepath): Load a WAV audio file from disk and return as an AudioSegment object.
-    sound.get_array_of_samples(): Return the raw audio samples from an AudioSegment object as an array of integers.
-    AudioSegment.apply_gain(): Apply a gain (amplification) to an AudioSegment object.
-    AudioSegment.export(): Export an AudioSegment object to a WAV file on disk.
-
-
-numpy:
-
-    numpy.array(): Create a numpy array from a list or array-like object.
-    numpy.argmax(): Find the index of the maximum value in a numpy array.
-    numpy.argmin(): Find the index of the minimum value in a numpy array.
-    numpy.abs(): Calculate the absolute value of a numpy array.
-    numpy.sum(): Calculate the sum of the values in a numpy array.
-    numpy.mean(): Calculate the mean (average) value of a numpy array.
-    numpy.std(): Calculate the standard deviation of a numpy array.
-"""
